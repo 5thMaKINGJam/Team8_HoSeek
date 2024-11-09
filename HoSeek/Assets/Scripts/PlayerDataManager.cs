dbe2acc9ff3256a970e53264af03c58c282b61a3
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-11-09 14:19:17 +0900

    스탯 설정/넘기기

--- HoSeek/Assets/Scripts/PlayerDataManager.cs
+++ HoSeek/Assets/Scripts/PlayerDataManager.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDataManager : MonoBehaviour
 {
-    int int_stat = 0;
-    int str_stat = 0;
-    int wis_stat = 0;
+    public static PlayerDataManager Instance;
 
-    void SetStat(int intel, int str, int wisdom){
+    public int int_stat { get; private set; } = 0;
+    public int str_stat { get; private set; } = 0;
+    public int wis_stat { get; private set; } = 0;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetStat(int intel, int str, int wisdom)
+    {
         int_stat = intel;
         str_stat = str;
         wis_stat = wisdom;
-    }
-    int GetIntStat(){
-        return int_stat;
-    }
-    int GetStrStat(){
-        return str_stat;
-    }
-    int GetWisStat(){
-        return wis_stat;
     }
 }
